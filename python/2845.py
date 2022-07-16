580cd54a5e8bfa2b36fa5b9e3f753fedc20b1d80
anojds <72495729+anojds@users.noreply.github.com>
2022-07-16 22:53:44 +0900

    Update 2845.py

--- python/2845.py
+++ python/2845.py
@@ -1,1 +1,10 @@
+if __name__ == '__main__':
+    value = input().split()
+    value2 = input().split()
+    value3 = ''
 
+    calc1 = int(value[0]) * int(value[1])
+
+    for i in range(0,len(value2)):
+        value3 = value3 + str(-(calc1-int(value2[i]))) + ' '
+    print(value3)
