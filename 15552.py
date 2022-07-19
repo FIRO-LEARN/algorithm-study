1b7955f33975a4c3c007a6a9c71ac82372ca4b23
anojds <72495729+anojds@users.noreply.github.com>
2022-07-19 21:14:28 +0900

    Delete 15552.py

--- 15552.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    value = int(input())
-
-    for i in range(value):
-        a, b = map(int, sys.stdin.readline().rstrip().split())
-        print(a + b)
